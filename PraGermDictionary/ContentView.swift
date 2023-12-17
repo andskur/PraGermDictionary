e099d40fb8bc97dcb73282ef2ddc5bc9af68426f
Andrew Skurlatov <a.skurlatov@gmail.com>
2023-12-17 22:39:08 +0100

    Initial Commit

---
+++ PraGermDictionary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PraGermDictionary
+//
+//  Created by Andrey Skurlatov on 17.12.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PraGermDictionary/PraGermDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraGermDictionaryApp.swift
+//  PraGermDictionary
+//
+//  Created by Andrey Skurlatov on 17.12.23..
+//
+
+import SwiftUI
+
+@main
+struct PraGermDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
